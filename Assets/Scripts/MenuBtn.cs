c13386665097fd2a95328df5b32f4b54c49e9fed
kosta080 <kosta.r5@gmail.com>
2023-05-20 18:11:52 +0300

    buttons sound fix

--- Assets/Scripts/MenuBtn.cs
+++ Assets/Scripts/MenuBtn.cs
@@ -1,21 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuBtn : MonoBehaviour
 {
-    public void StartGame()
+    public void StartGame() => StartCoroutine(startGame());
+    public void ExitGame() => StartCoroutine(exitGame());
+    public void GoToMainMenu() => StartCoroutine(goToMainMenu());
+
+    private const float ACTIONDELAY = 0.35f; //to give time for the click sound before switching scene
+
+    private IEnumerator startGame()
     {
         SoundManager.Instance.PlaySfx(SoundManager.Instance.buttonSound);
+        yield return new WaitForSeconds(ACTIONDELAY);
         SceneManager.LoadScene("Level", LoadSceneMode.Single);
     }
-    public void ExitGame()
+    private IEnumerator exitGame()
     {
         SoundManager.Instance.PlaySfx(SoundManager.Instance.buttonSound);
+        yield return new WaitForSeconds(ACTIONDELAY);
         Application.Quit();
     }
-    public void GoToMainMenu()
+    private IEnumerator goToMainMenu()
     {
         SoundManager.Instance.PlaySfx(SoundManager.Instance.buttonSound);
+        yield return new WaitForSeconds(ACTIONDELAY);
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 
